b455e7b98d70741a05a1425459b6a63e0721c445
Pavel Gabriel <alovak@gmail.com>
2016-03-07 21:08:20 +0300

    add scenario for declined charge

--- magento/test/spec/app_spec.rb
+++ magento/test/spec/app_spec.rb
@@ -28,7 +28,7 @@ describe 'store customer' do
     expect(page).to have_text("The configuration has been saved.")
   end
 
-  it 'pays for order' do
+  it 'pays successfully for order' do
     visit "/index.php/demo-product.html"
     expect(page).to have_text "Demo Description"
 
@@ -74,6 +74,85 @@ describe 'store customer' do
 
     # click_on "Continue"
     # sleep 1
+
+    click_on "Place Order"
+
+    sleep 2
+
+    expect(page).to have_content("YOUR ORDER HAS BEEN RECEIVED.")
+  end
+
+  it 'receives decline when charge declined' do
+    visit "/index.php/demo-product.html"
+    expect(page).to have_text "Demo Description"
+
+    click_on "Add to Cart"
+    expect(page).to have_content "Demo Product was added to your shopping cart."
+
+    visit "/index.php/checkout/onepage/"
+
+    choose "Checkout as Guest"
+    click_on "Continue"
+
+    fill_in "First Name", with: "Abdullah"
+    fill_in "Last Name", with: "Ahmed"
+    fill_in "Email Address", with: "start@payfort.com"
+    find("input[title='Street Address']").set "In the middle of something"
+    fill_in "City", with: "Dubai"
+    fill_in "Telephone", with: "+1-555-555-555"
+    select "United Arab Emirates", from: "Country"
+    fill_in "Zip", with: "12345"
+    click_on "Continue"
+    sleep 1
+
+    # shipping information
+    click_on "Continue"
+    sleep 1
+
+    choose "Credit / Debit Card"
+
+    click_on "Continue"
+
+    in_frame do
+      expect(page).to have_text("Secured with 128bit SSL encryption")
+      expect(find("#email").value).to eq("start@payfort.com")
+
+      fill_inputmask('#number', '4000000000000002')
+      fill_inputmask('#expiry', '11/22')
+      fill_inputmask('#cvc', '123')
+
+      click_on "Ok"
+      sleep 5
+    end
+
+    click_on "Place Order"
+
+
+    sleep 2
+    expect(page.driver.browser.switch_to.alert.text).to eq('Charge was declined. Please, contact you bank for more information or use a different card.')
+    page.driver.browser.switch_to.alert.accept
+
+    sleep 1
+
+    expect(page).to have_selector('li#opc-payment.active')
+
+    # second attempt
+    #
+    choose "Credit / Debit Card"
+
+    click_on "Continue"
+
+    in_frame do
+      expect(page).to have_text("Secured with 128bit SSL encryption")
+      expect(find("#email").value).to eq("start@payfort.com")
+
+      fill_inputmask('#number', '4242424242424242')
+      fill_inputmask('#expiry', '11/22')
+      fill_inputmask('#cvc', '123')
+
+      click_on "Ok"
+      sleep 5
+    end
 
     click_on "Place Order"
 
